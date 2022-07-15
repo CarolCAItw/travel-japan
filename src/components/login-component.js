2728a561b53bbad04d1d44ac3a6658bb5c7361a8
Carol Cai <e658956@gmail.com>
2022-07-15 18:32:55 +0800

    V1.1

--- src/components/login-component.js
+++ src/components/login-component.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router-dom";
 import AuthService from "../services/auth.service";
 
 const LoginComponent = (props) => {
